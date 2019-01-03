419068afdbf1a6a1c681423574752c8bebbf4d3d
olmil <leo.olmi@gmail.com>
2019-01-03 15:19:47 +0100

    roba

---
+++ raven.d.ts
@@ -0,0 +1,23 @@
+// RAVEN CLIENT TYPESCRIPT DEFINITIONS
+// v1.x.x
+
+/**
+ * send/retrieve state informations
+ */
+export function state(data?:any, o?:any): Promise<any>;
+/**
+ * subscribe to container events
+ */
+export function subscribe(fn:(data:any) => any, filter?:(data:any)=>boolean);
+/**
+ * unsubscribe from container events
+ */
+export function unsubscribe(fn:(data:any) => any);
+/**
+ * contaner interop state
+ */
+export const active: boolean;
+/**
+ * raven constants
+ */
+export const constants: any;
